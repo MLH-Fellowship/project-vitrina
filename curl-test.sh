e8832f3a90a93dc4d4a419c02625847cd65201bb
Cindy Liang <cindy.liang023@gmail.com>
2023-02-23 15:07:43 -0500

    Testing endpoints with curl

--- curl-test.sh
+++ curl-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#run flask first
+
 curl --request POST http://127.0.0.1:5000/api/timeline_post -d 'name=Cindy&email=cindyliang0127@gmail.com&content=Just added database to my portfolio site! :)'
 
 curl http://127.0.0.1:5000/api/timeline_post
