a57b9f8eb7b986d3af2ebfacc3502c6ed5d34f9d
Cindy Liang <cindy.liang023@gmail.com>
2023-02-24 21:27:59 -0500

    Updated deployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-git fetch && git reset origin/main --hard
- 
-source python3-virtualenv/bin/activate  
+cd MLH-project-vitrina
+git fetch
+git reset origin/main --hard
 
+source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
+systemctl daemon-reload
 systemctl restart myportfolio
