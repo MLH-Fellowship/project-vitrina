e6822bbd4af8ec1863a0f6f126de3a02fc9df364
Cindy Liang <cindy.liang023@gmail.com>
2023-02-28 16:17:57 -0500

    Updated automation for deployment

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -8,5 +8,5 @@ python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-systemctl daemon-reload
+# systemctl daemon-reload
 systemctl restart myportfolio
